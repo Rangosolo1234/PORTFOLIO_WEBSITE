8534ad8d13413270e17b425c6d735b0873abc5fd
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-03-20 01:19:11 +0300

    Updated Hero section

--- My_portfolio/src/components/HeroSection.vue
+++ My_portfolio/src/components/HeroSection.vue
@@ -11,8 +11,30 @@
                   <h1 class="pt-4 text-white font-bold text-4xl md:text-5xl lg:text-6xl">
                      Hi, I'm <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-pink-500">
                         Solomon
-                     </span>
+                     </span>😒
                   </h1>
+               </div>
+               <p class="text-gray-300 pt-8 text-center lg_text-left mx-auto max-w-xl">
+                  Disable or configure HMR connection (in cases where the HMR websocket must 
+                  use a different address from the http server).
+               </p>
+               <div class="flex items-center gap-3 pt-9 flex-col sm:flex-row sm:w-max sm:mx-auto lg:mx-0">
+                  <button class="px-6 md:px7 py-3 rounded-full relative groupw-full sm:w-max flex justify-center">
+                     <span class="absolute inset-0 rounded-3xl group-hover:scale-105 origin-center transition-all ease-in-out bg-primary 
+                     border-2 border-transparent"></span>
+                     <span class="relative flex items-center justify-center text-white">Hire Me</span>
+                  </button>
+                  <button class="border border-primary px-6 md-px-7 py-3 rounded-full relative group w-full sm:w-max flex justify-center">
+                     <div class="hover:scale-105 transition-all ease-in-out flex justify-center item-center relative">
+                     <div class="svg-container">
+                        <svg class="download-icon" width="18" height="22" viewBox="0 0 18 22" fill="none" xlmns="https://www.wl.org/2000/svg">
+                           <path >
+
+                           </path>
+                        </svg>
+                     </div>
+                     </div>
+                  </button>
                </div>
             </div>
          </div>
