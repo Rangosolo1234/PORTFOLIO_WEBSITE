69184acc3e061fdab8c9a74cc474b2f2ad166d43
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-03-19 18:39:41 +0300

    Navbar created

---
+++ My_portfolio/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+// Import components Asynchronously
+const NavBar = defineAsyncComponent(()=>import('@/components/NavBar.vue'))
+</script>
